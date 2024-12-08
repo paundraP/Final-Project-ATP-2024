e7e6ee38a383f27f2ad5cd7ee87797bff43e7ca1
idzaanafin <ahmadidanafin@gmail.com>
2024-12-08 22:41:59 +0700

    fix validation and testing

--- include/controller.h
+++ include/controller.h
@@ -5,15 +5,15 @@
 
 Playlist* findPlaylistByIndex(Playlist* playlist, int index);
 Playlist* createPlaylist(char name[]);
-Song* createSong(char new_singer[], char new_title[], char new_album[], double new_time);
-void addSongToPlaylist(Playlist* playlist, int index, const char* title, const char* singer, const char* album, double time);
+Song* createSong(char new_singer[], char new_title[], char new_album[], double new_time,char url[], char status[]);
+void addSongToPlaylist(Playlist* playlist, int index, char title[], char singer[], char album[], double time, char url[], char status[]);
 Playlist* addNewPlaylist(Playlist* playlist, char playlistname[]);
 int playlistExists(struct Playlist* playlist, char playlistname[]);
 int isSongInPlaylist(Song* songList, const char* title);
 Playlist* deleteSongFromPlaylist(Playlist* playlist, int index, char songName[]);
 Playlist* deletePlaylist(Playlist* playlist, int index);
 void savePlaylist(Playlist* playlist, int index);
-void listFileInPlaylistFolder();
+int listFileInPlaylistFolder();
 Playlist* findPlaylistByName(Playlist* head, char playlistName[]);
 Playlist* readPlaylist(struct Playlist* playlist, char playlistName[]);
 char* strip(char* str);
